5183fb236125f328fc34f3cd914b1be9a37bf0d3
Pedro Alves <quadrosalvespedro@gmail.com>
2020-05-10 13:18:19 -0300

    rev2
    Importação das libs iniciais + dados do problema

---
+++ Teste Ifood.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#importando as libs necessárias para a EDA
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# # 1- Análise exploratória de dados
+
+# In[2]:
+
+
+#importando a base de dados, como não é uma base muito grande, vamos usar diretamente do repositório do Github
+
+
+dados = pd.read_csv("https://raw.githubusercontent.com/ifood/ifood-data-business-analyst-test/master/ml_project1_data.csv")
+
+
+# In[3]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
